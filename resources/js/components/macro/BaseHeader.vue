0462ebaf22d0289a1710f2941f93e6ad750ad92b
TrvMarco <trevisanello.marco@gmail.com>
2022-07-26 16:41:07 +0200

    Aggiunto componenti vue js

---
+++ resources/js/components/macro/BaseHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+            <h2>MyBlog</h2>
+            </div>
+            <div class="col-6">
+                
+            </div>
+        </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'BaseHeader',
+}
+</script>
+
+<style>
+
+</style>
--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,10 +1,33 @@
 <template>
-    <h1>Prova Vue JS</h1>
+    <BaseHeader/>
+    <!-- <ul>
+        <li v-for="post in posts" :key="post.id">{{post.title}}</li>
+    </ul> -->
 </template>
 
 <script>
+import BaseHeader from '../components/macro/BaseHeader.vue';
+
 export default {
-    name: 'App'
+    name: 'App',
+    data(){
+        return{
+            posts:[]
+        };
+    },
+    components:{
+        BaseHeader,
+    },
+    created(){
+        axios.get('http://127.0.0.1:8000/api/posts')
+        .then((response) => {
+            console.log(response.data);
+            this.posts = response.data;
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
 }
 </script>
 
